15ae15b0530578e08ccd9227732ac2af48f6d165
robdodson <robdodson@google.com>
2021-03-07 14:55:56 -0800

    copy over config.yml

--- .eleventy.js
+++ .eleventy.js
@@ -41,6 +41,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/fonts');
   eleventyConfig.addPassthroughCopy('src/images');
   eleventyConfig.addPassthroughCopy('src/robots.txt');
+  eleventyConfig.addPassthroughCopy('src/admin/config.yml');
 
   const now = new Date();
 
